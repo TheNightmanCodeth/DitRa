5fd106bbc5b0a56d687f1be6d34ce0e2fbff5559
TheNightmanCodeth <joyod3@gmail.com>
2018-08-16 14:20:40 -0400

    Creates RedditHelper for dealing with authentication and network

--- lib/network/main.dart
+++ lib/network/main.dart
@@ -1,1 +1,101 @@
-// TBI
+import 'dart:async';
+import 'dart:io';
+
+import 'auth/SecretLoader.dart';
+import 'auth/Secret.dart';
+
+import 'package:draw/draw.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+
+class RedditHelper {  
+  StreamSubscription<String> _onUrlChanged;
+
+  Future<String> get _localPath async {
+    final dir = await getApplicationDocumentsDirectory();
+    return dir.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/cred.json');
+  }
+
+  Future<Secret> get _secrets async {
+    Secret secrets = await SecretLoader(secretPath: "assets/secrets.json").load();
+    return secrets;
+  }
+
+  writeCredentials(String credentials) async {
+    final file = await _localFile;
+    file.writeAsString(credentials);
+  }
+
+  Future<String> readCredentials() async {
+    File file = await _localFile;
+    if (FileSystemEntity.typeSync(file.path) == FileSystemEntityType.notFound) {
+      return "";
+    } else {
+      try {
+        final file = await _localFile;
+        String creds = await file.readAsString();
+        return creds;
+      } catch (e) {
+        return "";
+      }
+    }
+  }
+
+  Future<Reddit> login() async {
+    final secret = await _secrets; 
+    FlutterWebviewPlugin fwp = FlutterWebviewPlugin();
+    _onUrlChanged =
+    fwp.onUrlChanged.listen((url) async {
+      if (url.contains("state=ditra&code=")) {
+        print("The troll toll has been paid");
+        final secret = await _secrets;
+        final reddit =
+        Reddit.createWebFlowInstance(
+          userAgent: "ditra",
+          clientId: secret.clientId,
+          clientSecret: secret.clientSecret,
+          redirectUri: Uri.parse(secret.redirectUri),
+        );
+        String accessCode = Uri.parse(url).queryParameters["code"];
+        final WebAuthenticator auth = reddit.auth;
+        auth.url(['*'], 'ditra');
+        await auth.authorize(accessCode);
+        await writeCredentials(auth.credentials.toJson());
+        return reddit;
+      }
+    });
+    await fwp.launch(secret.oauthUrl);
+  }
+
+  Future<Reddit> getAnonClient(Secret secret) async {
+    Reddit anon = await Reddit.createReadOnlyInstance(
+      clientId: secret.clientId,
+      clientSecret: secret.clientSecret,
+      userAgent: "ditra:plays2:v0.0 (by /u/plays2",      
+    );
+    return anon;
+  }
+
+  Future<Reddit> getRedditClient() async {
+    final secret = await _secrets; 
+    var credentials = await readCredentials();
+    if (credentials == "" || credentials == null) {
+      print("Getting unauthorized client...");
+      Reddit client = await getAnonClient(secret);
+      return client;
+    } else {           
+      Reddit client = await Reddit.restoreAuthenticatedInstance(
+        credentials,
+        clientId: secret.clientId,
+        clientSecret: secret.clientSecret,
+        userAgent: secret.userAgent,
+      );
+      return client;
+    }
+  }
+}
