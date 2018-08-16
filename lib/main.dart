9eef34228d32dd8041fa2a2e0a533b94a58d035a
TheNightmanCodeth <joyod3@gmail.com>
2018-08-16 14:20:09 -0400

    Implements early frontpage loading using readonly and authorized reddit

--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,16 @@ import 'dart:async';
 
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 import 'package:flutter/material.dart';
+import 'package:draw/draw.dart';
 
-//import 'network/main.dart';
+import 'network/main.dart';
+import 'network/auth/SecretLoader.dart';
 
-void main() => runApp(DitRa());
-const oauthUrl = "";
+void main() => runApp(MaterialApp(
+  title: "DitRa",
+  home: DitRaHome(),
+));
 
-class DitRa extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      routes: {
-        '/': (_) => const DitRaHome(title: 'DitRa'),
-      },
-    );
-  }
-}
 
 class DitRaHome extends StatefulWidget {
   const DitRaHome({Key key, this.title}) : super(key: key);
@@ -29,25 +23,30 @@ class DitRaHome extends StatefulWidget {
 
 class _DitRaHomeState extends State<DitRaHome> {
   final flutterWebViewPlugin = FlutterWebviewPlugin();
+  RedditHelper redditHelper = RedditHelper();
+  Reddit reddit;
+  StreamSubscription<String>_onStateChanged;
+  StreamSubscription _onDestroy;
 
   @override
   void initState() {
     super.initState();
-
-    flutterWebViewPlugin.close();
-    StreamSubscription<String>_onStateChanged = 
-    flutterWebViewPlugin.onUrlChanged.listen((String url) {
-      if (mounted) {
-        if (url.contains("?state=ditra&code=")) {
-          var code = Uri.parse(url).queryParameters["code"].toString();
-          flutterWebViewPlugin.close();
-        }
-      }
-    });
-
+    
   }
-  
-  
+
+  @override
+  void dispose() {
+    _onStateChanged.cancel();
+    _onDestroy.cancel();
+    flutterWebViewPlugin.dispose();
+    super.dispose();
+  }
+
+  Future<List<UserContent>> postList() async {
+    reddit = await redditHelper.getRedditClient();
+    print(reddit.readOnly.toString());
+    return reddit.front.hot().toList();
+  }
 
   @override
     Widget build(BuildContext context) {
@@ -55,15 +54,45 @@ class _DitRaHomeState extends State<DitRaHome> {
         appBar: AppBar(
           title: Text('DitRa'),
         ),
-        body: Container(
-          child: Center(
-            child: RaisedButton(
-              child: Text('Login'),
-              onPressed: () {
-                flutterWebViewPlugin.launch(oauthUrl);
-              }              
-            ),
+        drawer: Drawer(
+          child: Column(
+            children: <Widget>[
+              DrawerHeader(
+                child: Text("Hello"),
+              ),
+              ListTile(
+                leading: Icon(Icons.account_circle),
+                title: Text("Login"),
+                onTap: () async {
+                  reddit = await redditHelper.login();
+                }
+              ),
+            ],
           ),
+        ),
+        body: Container(
+          child: FutureBuilder(
+            future: postList(),
+            builder: (context, snapshot) {
+              switch(snapshot.connectionState) {
+                case ConnectionState.waiting: return Text('Loading...');
+                default:
+                  if (snapshot.hasError) {
+                    print(snapshot.error.toString());
+                    return Text('whoopsiee');
+                  } else {
+                    return ListView.builder(
+                      itemBuilder: (context, index) {
+                        Submission submission = snapshot.data[index];
+                        return Card(
+                          child: Text(submission.title),
+                        );
+                      },
+                    );
+                  }
+              }
+            },
+          )
         ),
       );
     }
