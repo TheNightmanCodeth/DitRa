637125d1998e18e15769a89eb41a908d109d1f6d
TheNightmanCodeth <joyod3@gmail.com>
2018-08-20 13:02:28 -0400

    Adds upvote button to post views and impl upvote logic

--- lib/views/widgets/image_post_card_widget.dart
+++ lib/views/widgets/image_post_card_widget.dart
@@ -3,36 +3,77 @@
 import 'package:flutter/material.dart';
 import 'package:draw/draw.dart';
 
-import 'flutter_webview_widget.dart';
+import '../ImageView.dart';
 
 class ImagePost extends StatefulWidget {
 
-  ImagePost(this.post);
+  ImagePost(this.post, this.score);
 
   final Submission post;
+  final score;
 
   @override
-  _ImagePostState createState() => _ImagePostState(post);
+  _ImagePostState createState() => _ImagePostState(post, score);
 }
 
 class _ImagePostState extends State<ImagePost> {
 
-  _ImagePostState(this.post);
+  _ImagePostState(this.post, this.score);
   Submission post;
+  int score;
+
+  @override
+  void initState() {
+    super.initState();
+    score = post.score;
+  }
 
   @override
   Widget build(BuildContext context) { 
-    return Card(
+    return Card(      
       elevation: 4.0,
-      margin: EdgeInsets.only(top: 8.0, bottom: 8.0),
+      margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
       child: Container(
         child: Column(
           children: <Widget>[
             ListTile(
               title: Text(post.title),
               subtitle: Text("${post.author} - ${post.subreddit.displayName}"),
+              trailing: Text(score.toString()),
             ),
-            Image.network(post.url.toString())
+            InkWell(
+              child: Image(
+                image: NetworkImage(post.thumbnail.toString()),
+                fit: BoxFit.fitWidth,
+                width: 500.0,
+              ),
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(builder: (ctx) => ImageView(post.url.toString()))
+                );
+              },
+            ),
+            Container(
+              height: 45.0,
+              child: Row(
+                // Upvote
+                children: <Widget>[
+                  Padding(
+                    child: InkWell(
+                      radius: 20.0,
+                      child: Icon(Icons.arrow_upward),
+                      onTap: () async {
+                        await post.upvote();                        
+                        setState(() {
+                          score = score + 1;
+                        });
+                      },
+                    ), 
+                    padding: EdgeInsets.all(10.0),
+                  ),                  
+                ],
+              ),
+            ),
           ],
         ),
       ),
--- lib/views/widgets/text_post_card_widget.dart
+++ lib/views/widgets/text_post_card_widget.dart
@@ -10,31 +10,73 @@ class TextPost extends StatefulWidget {
   TextPost(this.post);
 
   final Submission post;
+  final int score;
 
   @override
-  _TextPostState createState() => _TextPostState(post);
+  _TextPostState createState() => _TextPostState(post, score);
 }
 
 class _TextPostState extends State<TextPost> {
 
-  _TextPostState(this.post);
+  _TextPostState(this.post, this.score);
   Submission post;
+  int score;
+
+  @override
+    void initState() {
+      score = post.score;
+      super.initState();
+    }
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-        margin: EdgeInsets.only(top: 8.0, bottom: 8.0),
-        child: Container(
-          child: Column(
-            children: <Widget>[
-              ListTile(
-                title: Text(post.title),               
-                subtitle: Text("${post.author} - ${post.subreddit.displayName}"), 
+    return Card(      
+      elevation: 4.0,
+      margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+      child: Container(
+        child: Column(
+          children: <Widget>[
+            ListTile(
+              title: Text(post.title),
+              subtitle: Text("${post.author} - ${post.subreddit.displayName}"),
+              trailing: Text(score.toString()),
+            ),
+            Builder(
+              builder: (ctx) {
+                if (post.selftext == null || post.selftext == "") {
+                  return Container(width: 0.0, height: 0.0);
+                } else {
+                  return Padding(
+                    padding: EdgeInsets.all(18.0),
+                    child: Text(post.selftext),
+                  );
+                }
+              }
+            ),
+            Container(
+              height: 45.0,
+              child: Row(
+                // Upvote
+                children: <Widget>[
+                  Padding(
+                    child: InkWell(
+                      radius: 20.0,
+                      child: Icon(Icons.arrow_upward),
+                      onTap: () async {                                               
+                        await post.upvote();                        
+                        setState(() {
+                          score = score + 1;
+                        });
+                      },
+                    ), 
+                    padding: EdgeInsets.all(10.0),
+                  ),                  
+                ],
               ),
-              Text(post.selftext),
-            ],
-          ),
+            ),
+          ],
         ),
-      );
+      ),
+    );
   }
 }
