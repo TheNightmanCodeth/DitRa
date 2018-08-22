733df0c82baa367470b6620e2ac6feffa3488c4b
TheNightmanCodeth <joyod3@gmail.com>
2018-08-22 14:48:58 -0400

    Fix copy/paste error

--- lib/views/widgets/text_post_card_widget.dart
+++ lib/views/widgets/text_post_card_widget.dart
@@ -1,8 +1,8 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:draw/draw.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
+
+import '../CommentsView.dart';
 
 class TextPost extends StatefulWidget {
 
