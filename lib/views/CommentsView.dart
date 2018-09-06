0e1e42b3b6c7836cb1d6995df986716a0787ec39
TheNightmanCodeth <joyod3@gmail.com>
2018-09-06 14:10:51 -0400

    Remove appbar from comments view in favor of header text like subreddit view

--- lib/views/CommentsView.dart
+++ lib/views/CommentsView.dart
@@ -35,48 +35,58 @@ class _CommentsViewState extends State<CommentsView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Comments"),
-      ),
-      body: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-        Expanded(
-          child: ListView(
-            children: <Widget>[
-              Builder(builder: (c) {
-                if (submission.isSelf) {
-                  return TextPost(submission);
-                } else {
-                  return ImagePost(submission, submission.score);
-                }
-              }),
-              Container(height: 16.0),
-              FutureBuilder(
-                  future: initComments(),
-                  builder: (c, snapshot) {
-                    if (snapshot.hasData) {
-                      CommentForest comments = snapshot.data;
-                      return Column(
-                        children: List.generate(comments.length, 
-                          (index) {
-                            var thisComment = comments[index];
-                            return CommentWidget(thisComment);
-                          }
-                        ),
-                      );
-                    } else {
-                      if (snapshot.hasError) {
-                        print(snapshot.error);
-                      }
-                      return Container(
-                        height: 0.0,
-                        width: 0.0,
-                      );
-                    } 
-                  })
-            ],
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: ListView(
+              children: <Widget>[
+                Container(
+                  child: Text(
+                    "Comments",
+                    style: TextStyle(
+                      fontFamily: "Raleway-SemiBold",
+                      fontSize: 32.0
+                    ),
+                  ),
+                  padding: EdgeInsets.only(top: 32.0, bottom: 16.0, left: 16.0, right: 16.0),
+                ),
+                Builder(builder: (c) {
+                  if (submission.isSelf) {
+                    return TextPost(submission);
+                  } else {
+                    return ImagePost(submission, submission.score);
+                  }
+                }),
+                Container(height: 16.0),
+                FutureBuilder(
+                    future: initComments(),
+                    builder: (c, snapshot) {
+                      if (snapshot.hasData) {
+                        CommentForest comments = snapshot.data;
+                        return Column(
+                          children: List.generate(comments.length, 
+                            (index) {
+                              var thisComment = comments[index];
+                              return CommentWidget(thisComment);
+                            }
+                          ),
+                        );
+                      } else {
+                        if (snapshot.hasError) {
+                          print(snapshot.error);
+                        }
+                        return Container(
+                          height: 0.0,
+                          width: 0.0,
+                        );
+                      } 
+                    })
+              ],
+            ),
           ),
-        ),
-      ]),
+        ]
+      ),
     );
   }
 }
